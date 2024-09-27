0d60de50a81a98769f7e464af97e28204a07afd8
hongnhat195 <133624997+hNhat195@users.noreply.github.com>
2024-09-27 14:08:09 +0700

    add sh config

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ set -e
 echo "run db migration"
 source app.env
 ls 
+cat app.env
+echo "$DB_SOURCE"
 /app/migrate -path db/migration -database "$DB_SOURCE" -verbose up
 echo "start the app"
 exec "$@"
