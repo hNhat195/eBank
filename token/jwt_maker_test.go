f92450e23293a5b8c9efdce7f8c2940d966effd9
hongnhat195 <133624997+hNhat195@users.noreply.github.com>
2024-09-30 10:00:45 +0700

    change token test

--- token/jwt_maker_test.go
+++ token/jwt_maker_test.go
@@ -36,12 +36,11 @@ func TestExpiredJWTToken(t *testing.T) {
 	maker, err := NewJWTMaker(util.RandomString(32))
 	require.NoError(t, err)
 
-	token, payload, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	token, _, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
 	require.NoError(t, err)
 	require.NotEmpty(t, token)
-	require.NotEmpty(t, payload)
 
-	// payload, err := maker.VerifyToken(token)
+	payload, err := maker.VerifyToken(token)
 	require.Error(t, err)
 	require.EqualError(t, err, ErrExpiredToken.Error())
 	require.Nil(t, payload)
